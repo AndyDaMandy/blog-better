e227ac1cad26e90b31569a988eb4204abdae0f0b
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-06-28 19:32:21 -0400

    adding user auth res

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   root "articles#index"
+  #Users routes
+  resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
 
   resources :articles do
     resources :comments
   end
+  #Sessions routes
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
