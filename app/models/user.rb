b6e6d69e23c551a59b787bd1211d1707b6967255
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-07-07 13:27:50 -0400

    added params to user gen

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :username, presence: true, length: { minimum: 5}
+    validates :password, presence: true, length: { minimum: 10 }
 end
